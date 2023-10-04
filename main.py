554b01c85f2219061e8971dbc14f682923c75ad9
jeesup0103 <jeesup0103@gmail.com>
2023-10-04 18:58:12 +0900

    self

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from typing import List
 
 
 def twoSum(self, nums: List[int], target: int) -> List[int]:
-    num_indices = {}
+    index = {}
 
     for i, num in enumerate(nums):
-        complement = target - num
-        if complement in num_indices:
-            return [num_indices[complement], i]
-        num_indices[num] = i
+        operand = target - num
+        if operand in index:
+            return [index[operand], i]
+        index[num] = i
 
-    return num_indices
+    return index
